f116cca0e63cb77e19e3574e03c460b4ab04e4f1
Zoltán <weper95@gmail.com>
2018-02-10 21:41:57 +0100

    IP Lookup test

---
+++ commands/iplookup.py
@@ -0,0 +1,17 @@
+import _variables
+import xml.etree.ElementTree as ET
+
+cmd = "iplookup"
+
+def test(client, message):
+    if message.content.startswith(_variables.prefix + cmd + ' '):
+        user_message = str(message.content).replace(_variables.prefix + cmd + ' ', '')
+
+        tree = ET.parse('http://ip-api.com/xml/' + 'user_message')
+        root = tree.getroot()
+
+        country = root[0][1].text
+        city = root[0][5].text
+
+        if len(str(message.content)) > len(_variables.prefix + cmd + ' '):
+            yield from client.send_message(message.channel, 'Country: ' + country + " | City: " + city)
